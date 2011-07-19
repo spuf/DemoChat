62627142bf99094735ca559ccfefc0bb148a46a0
Arseniy Razin <spufru@gmail.com>
2011-07-19 19:20:41 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,91 @@
+import logging
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import os.path
+import asyncmongo
+import time
+import functools
+
+from tornado.options import define, options
+
+define('port', default=81, help='run on the given port', type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        self.db = asyncmongo.Client(pool_id='mydb', host='127.0.0.1', port=27017, dbname='test')
+        handlers = [
+                (r'/', MainHandler),
+                (r'/add_message', AddHandler),
+                (r'/chatsocket', ChatSocketHandler)
+        ]
+        settings = dict(
+            cookie_secret='43oETzKXQAGaYdk6fd8fG1kJFuYh7EQnp2XdTP1o/Vo=',
+            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+            xsrf_cookies=True,
+            autoescape=None
+            #debug=True
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.application.db.messages.find({}, limit=50, sort=[('time', -1)], callback=self.on_response)
+
+    def on_response(self, response, error):
+        if error:
+            raise tornado.web.HTTPError(500)
+        self.render('index.html', messages=response[::-1])
+
+class AddHandler(tornado.web.RequestHandler):
+    def post(self):
+        logging.info('Post message')
+        self.write('Your browser doesn\'t support JavaScript or WebSockets or Flash.');
+
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    waiters = set()
+
+    def open(self):
+        ChatSocketHandler.waiters.add(self)
+        logging.info('Add waiter')
+
+    def on_close(self):
+        ChatSocketHandler.waiters.remove(self)
+        logging.info('Remove waiter')
+
+    @classmethod
+    def send_updates(cls, result, error, chat):
+        if error:
+            raise tornado.web.HTTPError(500)
+        logging.info('Sending message to %d waiters', len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                logging.error('Error sending message', exc_info=True)
+
+    def on_message(self, message):
+        logging.info('Got message %r', message)
+        parsed = tornado.escape.json_decode(message)
+        chat = {
+            'body': parsed['body'],
+            'time': time.time()
+        }
+        chat['html'] = self.render_string('message.html', message=chat)
+        callback = functools.partial(ChatSocketHandler.send_updates, chat=chat)
+        self.application.db.messages.insert(chat, callback=callback)
+
+
+def main():
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    main()
---
+++ static/chat.js
@@ -0,0 +1,68 @@
+WEB_SOCKET_SWF_LOCATION = "/static/WebSocketMain.swf";
+WEB_SOCKET_DEBUG = false;
+
+$(document).ready(function() {
+    if (!window.console) window.console = {};
+    if (!window.console.log) window.console.log = function() {};
+
+	$('#post').attr('disabled', 'disabled');
+	
+    $("#messageform").live("submit", function() {
+        newMessage($(this));
+        return false;
+    });
+    $("#messageform").live("keypress", function(e) {
+        if (e.keyCode == 13) {
+            newMessage($(this));
+            return false;
+        }
+    });
+    $("#message").select();
+    updater.start();
+});
+
+function newMessage(form) {
+    var message = form.formToDict();
+    updater.socket.send(JSON.stringify(message));
+    form.find("input[type=text]").val("").select();
+}
+
+jQuery.fn.formToDict = function() {
+    var fields = this.serializeArray();
+    var json = {}
+    for (var i = 0; i < fields.length; i++) {
+        json[fields[i].name] = fields[i].value;
+    }
+    if (json.next) delete json.next;
+    return json;
+};
+
+var updater = {
+    socket: null,
+
+    start: function() {
+		updater.socket = new WebSocket('ws://'+document.location.host+'/chatsocket');
+		
+		updater.socket.onmessage = function(event) {
+			updater.showMessage(JSON.parse(event.data));
+		}
+		
+		updater.socket.onopen = function(event) {
+            $('#post').removeAttr('disabled');
+        }
+		
+		updater.socket.onclose = function () {
+			if (confirm('Socket disconnected.\nReload this page?'))
+				document.location.reload();
+		}
+    },
+
+    showMessage: function(message) {
+        var existing = $("#m" + message.id);
+        if (existing.length > 0) return;
+        var node = $(message.html);
+        node.hide();
+        $("#inbox").append(node);
+        node.slideDown();
+    }
+};
